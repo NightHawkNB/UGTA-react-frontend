8a5391aab1ddb828ab89c4effcea779c358e0992
NightHawkNB <nipunbathiya1256@outlook.com>
2025-07-31 11:06:28 +0530

    [NEW] States and variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,22 @@ import "./App.css";
 import { FeatureItem } from "./components/feature.jsx"
 import { Header } from "./components/header.jsx";
 
+import { useState } from "react";
+
 function App() {
   
+  let main_title = "Student Management System";
+
+  const [count, setCount] = useState(0);
+
+  function incrementCounter() {
+    setCount(count+1);
+  }
+
+  function decrementCounter() {
+    setCount(count-1);
+  }
+
   return (
     <div className="main-container">
       
@@ -19,14 +33,22 @@ function App() {
 
         <hr/>
 
+        <div className="conatiner" style={{ display: "flex", gap: "10px", justifyContent: "start", alignItems: "center" }}>
+          <button onClick={incrementCounter}>+</button>
+          Counter : {count}
+          <button onClick={decrementCounter}>-</button>
+        </div>
+
+        <hr />
+
         <div className="feature-section">
           <h2 className="section-heading">Features</h2>
 
           <div className="feature-grid">
-            <FeatureItem title="feature 1" index={1} />
-            <FeatureItem title="feature 2" index={2} />
-            <FeatureItem title="feature 3" index={3} />
-            <FeatureItem title="feature 4" index={4} />
+            <FeatureItem title="feature 1" index={1} main={main_title} />
+            <FeatureItem title="feature 2" index={2} main={main_title} />
+            <FeatureItem title="feature 3" index={3} main={main_title} />
+            <FeatureItem title="feature 4" index={4} main={main_title} />
           </div>
         </div>
 
--- src/components/feature.jsx
+++ src/components/feature.jsx
@@ -1,8 +1,9 @@
 
 function FeatureItem(props) {
+
     return (
         <div className="feature-item">
-            { props.title } { props.index }
+            { props.title }
         </div>
     )
 }
